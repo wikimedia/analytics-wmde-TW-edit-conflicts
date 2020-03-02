c481d5c41208d1fa735851a3aa4fa1741ca3a781
Adam Wight <adam.wight@wikimedia.de>
2020-03-02 22:37:24 +0100

    Try to snip smaller datasets

--- src/main/scala/ConflictApp.scala
+++ src/main/scala/ConflictApp.scala
@@ -2,9 +2,17 @@ import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.SparkSession
 
 
+/**
+ * Take a sample of edit conflict logs, and gather metadata about the conflicting revisions.
+ *
+ * TODO:
+ *   - Is there a way to do a single scan of revisions?  Are we doing three here?
+ *   - Don't need all the left-hand columns when joining, maybe these can be a pure "where" instead.
+ */
 object ConflictApp {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Conflict Query").getOrCreate()
+    import spark.implicits._
 
     val conflicts = spark.sql(
       """
@@ -19,75 +27,94 @@ object ConflictApp {
         |  useragent.os_family
         |from event.twocolconflictconflict
         |where year = 2020 and month = 2
+        |-- FIXME: pilot run
+        |limit 2
         |""".stripMargin
       )
 
-    val base_revs = spark.sql(
-      """
-        |select
-        |  event_timestamp as base_timestamp,
-        |  event_comment as base_comment,
-        |  event_user_text as base_user,
-        |  page_id,
-        |  page_namespace,
-        |  page_title,
-        |  revision_id as base_rev_id,
-        |  wiki_db as base_wiki
-        |from wmf.mediawiki_history
-        |where
-        |  event_entity = 'revision'
-        |  and event_type = 'create'
-        |""".stripMargin
+    val base_revs = spark
+      .sql(
+        """
+          |select
+          |  event_timestamp as base_timestamp,
+          |  event_comment as base_comment,
+          |  event_user_text as base_user,
+          |  page_id,
+          |  page_namespace,
+          |  page_title,
+          |  revision_id as base_rev_id,
+          |  wiki_db as base_wiki
+          |from wmf.mediawiki_history
+          |where
+          |  event_entity = 'revision'
+          |  and event_type = 'create'
+          |""".stripMargin
+      )
+      .join(
+        conflicts,
+        $"baseRevisionId" === $"base_rev_id"
+          && $"wiki" === $"base_wiki"
+      )
+
+    val other_revs = spark
+      .sql(
+        """
+          |select
+          |  event_timestamp as other_timestamp,
+          |  event_comment as other_comment,
+          |  event_user_text as other_user,
+          |  revision_id as other_rev_id,
+          |  wiki_db as other_wiki
+          |from wmf.mediawiki_history
+          |where
+          |  event_entity = 'revision'
+          |  and event_type = 'create'
+          |""".stripMargin
+      ).join(
+        conflicts,
+        $"latestRevisionId" === $"other_rev_id"
+          && $"wiki" === $"other_wiki"
     )
 
-    val other_revs = spark.sql(
-      """
-        |select
-        |  event_timestamp as other_timestamp,
-        |  event_comment as other_comment,
-        |  event_user_text as other_user,
-        |  revision_id as other_rev_id,
-        |  wiki_db as other_wiki
-        |from wmf.mediawiki_history
-        |where
-        |  event_entity = 'revision'
-        |  and event_type = 'create'
-        |""".stripMargin
-    )
+    val next_revs = spark
+      .sql(
+        """
+          |select
+          |  event_timestamp as next_timestamp,
+          |  event_comment as next_comment,
+          |  event_user_text as next_user,
+          |  revision_id as next_rev_id,
+          |  revision_parent_id as next_parent_id,
+          |  wiki_db as next_wiki
+          |from wmf.mediawiki_history
+          |where
+          |  event_entity = 'revision'
+          |  and event_type = 'create'
+          |""".stripMargin
+      ).join(
+        conflicts,
+        $"latestRevisionId" === $"next_parent_id"
+          && $"wiki" === $"next_wiki"
+      )
 
-    val next_revs = spark.sql(
-      """
-        |select
-        |  event_timestamp as next_timestamp,
-        |  event_comment as next_comment,
-        |  event_user_text as next_user,
-        |  revision_id as next_rev_id,
-        |  revision_parent_id as next_parent_id,
-        |  wiki_db as next_wiki
-        |from wmf.mediawiki_history
-        |where
-        |  event_entity = 'revision'
-        |  and event_type = 'create'
-        |""".stripMargin
-    )
-
+    // Recombine datasets into flat output rows.
     val conflict_details = conflicts
       .join(
         base_revs,
-        conflicts("baseRevisionId") === base_revs("base_rev_id")
-          and conflicts("wiki") === base_revs("base_wiki"),
+        conflicts("baseRevisionId") === $"base_rev_id"
+          && conflicts("wiki") === $"base_wiki",
         "left"
       )
       .join(
         other_revs,
-        conflicts("latestRevisionId") === other_revs("other_rev_id")
-          and conflicts("wiki") === other_revs("other_wiki"),
+        conflicts("latestRevisionId") === $"other_rev_id"
+          && conflicts("wiki") === $"other_wiki",
         "left"
       )
       .join(
         next_revs,
-        conflicts("latestRevisionId") === next_revs("next_parent_id")
-          and conflicts("wiki") === next_revs("next_wiki"),
+        conflicts("latestRevisionId") === $"next_parent_id"
+          && conflicts("wiki") === $"next_wiki",
         "left"
       )
 
@@ -97,6 +124,7 @@ object ConflictApp {
       .option("header", "true")
       .mode(SaveMode.Overwrite)
       .csv("/tmp/awight/conflicts")
+
     spark.stop()
   }
 }
