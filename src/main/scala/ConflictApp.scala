e9fc536d2c8e8b21bb77bd52d1b3910c196d9d91
Adam Wight <adam.wight@wikimedia.de>
2020-03-02 14:29:54 +0100

    Play with join

--- src/main/scala/ConflictApp.scala
+++ src/main/scala/ConflictApp.scala
@@ -1,24 +1,50 @@
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.SparkSession
+
 
 object ConflictApp {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Conflict Query").getOrCreate()
-    val conflicts = spark.sql("""
-              |select
-              |   webhost,
-              |   wiki,
-              |   event.baseRevisionId,
-              |   event.latestRevisionId,
-              |   event.editCount,
-              |   replace(event.textUser, '\n', '\\n')
-              |   useragent.browser_family,
-              |   useragent.os_family
-              |from event.twocolconflictconflict
-              |where year = 2020 and month = 2
-              |""".stripMargin
-        )
-    val base_revs = 
-    conflicts
+
+    val conflicts = spark.sql(
+      """
+        |select
+        |  webhost,
+        |  wiki,
+        |  event.baseRevisionId,
+        |  event.latestRevisionId,
+        |  event.editCount,
+        |  replace(event.textUser, '\n', '\\n'),
+        |  useragent.browser_family,
+        |  useragent.os_family
+        |from event.twocolconflictconflict
+        |where year = 2020 and month = 2
+        |""".stripMargin
+      )
+    val revisions = spark.sql(
+      """
+        |select
+        |  event_timestamp,
+        |  event_comment,
+        |  event_user_text,
+        |  page_id,
+        |  page_namespace,
+        |  page_title,
+        |  revision_id,
+        |  wiki_db
+        |from wmf.mediawiki_history
+        |where
+        |  event_entity = 'revision'
+        |  and event_type = 'create'
+        |""".stripMargin
+      )
+
+    val conflict_details = conflicts.join(
+      revisions,
+      conflicts("baseRevisionId") === revisions("revision_id")
+        and conflicts("wiki") === revisions("wiki_db"))
+
+    conflict_details
       .coalesce(1)
       .write
       .option("header", "true")
