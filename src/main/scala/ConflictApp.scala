63087353cab0dfb94c81350668f748de387f7997
Adam Wight <adam.wight@wikimedia.de>
2020-03-02 14:45:37 +0100

    Get crazy with joins

--- src/main/scala/ConflictApp.scala
+++ src/main/scala/ConflictApp.scala
@@ -21,28 +21,75 @@ object ConflictApp {
         |where year = 2020 and month = 2
         |""".stripMargin
       )
-    val revisions = spark.sql(
+
+    val base_revs = spark.sql(
       """
         |select
-        |  event_timestamp,
-        |  event_comment,
-        |  event_user_text,
+        |  event_timestamp as base_timestamp,
+        |  event_comment as base_comment,
+        |  event_user_text as base_user,
         |  page_id,
         |  page_namespace,
         |  page_title,
-        |  revision_id,
+        |  revision_id as base_rev_id,
         |  wiki_db
         |from wmf.mediawiki_history
         |where
         |  event_entity = 'revision'
         |  and event_type = 'create'
         |""".stripMargin
-      )
+    )
 
-    val conflict_details = conflicts.join(
-      revisions,
-      conflicts("baseRevisionId") === revisions("revision_id")
-        and conflicts("wiki") === revisions("wiki_db"))
+    val other_revs = spark.sql(
+      """
+        |select
+        |  event_timestamp as other_timestamp,
+        |  event_comment as other_comment,
+        |  event_user_text as other_user,
+        |  revision_id as other_rev_id,
+        |  wiki_db
+        |from wmf.mediawiki_history
+        |where
+        |  event_entity = 'revision'
+        |  and event_type = 'create'
+        |""".stripMargin
+    )
+
+    val next_revs = spark.sql(
+      """
+        |select
+        |  event_timestamp as next_timestamp,
+        |  event_comment as next_comment,
+        |  event_user_text as next_user,
+        |  revision_id as next_rev_id,
+        |  revision_parent_id as next_parent_id,
+        |  wiki_db
+        |from wmf.mediawiki_history
+        |where
+        |  event_entity = 'revision'
+        |  and event_type = 'create'
+        |""".stripMargin
+    )
+
+    val conflict_details = conflicts
+      .join(
+        base_revs,
+        conflicts("baseRevisionId") === base_revs("revision_id")
+          and conflicts("wiki") === base_revs("wiki_db"),
+        "left outer"
+      )
+      .join(
+        other_revs,
+        conflicts("latestRevisionId") === other_revs("other_rev_id")
+          and conflicts("wiki") === other_revs("wiki_db"),
+        "left outer"
+      )
+      .join(
+        other_revs,
+        conflicts("latestRevisionId") === next_revs("next_parent_id")
+          and conflicts("wiki") === next_revs("wiki_db"),
+        "left outer"
+      )
 
     conflict_details
       .coalesce(1)
