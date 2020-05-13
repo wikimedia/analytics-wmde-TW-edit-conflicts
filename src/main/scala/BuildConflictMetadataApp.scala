aa78fcae5efd0c376d85b6c45e460d43a55406bf
Adam Wight <adam.wight@wikimedia.de>
2020-05-13 13:47:39 +0200

    Move sample window to April
    Our small default was launched 25 March, so I'm moving the window
    to capture newer data.
    
    Bug: T252507
    Change-Id: Ie3deea037a7a19669e2eaf910c6c7495093783f5

--- src/main/scala/BuildConflictMetadataApp.scala
+++ src/main/scala/BuildConflictMetadataApp.scala
@@ -3,7 +3,7 @@
  */
 object BuildConflictMetadataApp extends SparkSessionWrapper {
   def main(args: Array[String]): Unit = {
-    val conflicts = DebugTable("conflicts", QueryConflictPeriod(2020, 3))
+    val conflicts = DebugTable("conflicts", QueryConflictPeriod(year = 2020, month = 4))
     val conflict_rev_details = DebugTable("conflict_rev_details", QueryConflictRevisionMetadata(conflicts, 2020))
     val edit_attempt_steps = DebugTable("conflict_edit_steps", QueryConflictEditAttemptSteps(conflicts, 2020))
 
